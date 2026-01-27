a108a4e33720845f133f2556f6af7a5f3ea7d418
何寸杰 <hecunjie@xiaohongshu.com>
2026-01-28 02:41:05 +0800

    修改启动脚本

--- scripts/rl_plus_kd.sh
+++ scripts/rl_plus_kd.sh
@@ -15,6 +15,9 @@ set -ex
 # will prevent ray from buffering stdout/stderr
 export PYTHONBUFFERED=16
 
+export WANDB_API_KEY="522a32e0a2b1b6781aabe86e432e96c99f5ca4f7"
+
+
 NVLINK_COUNT=$(nvidia-smi topo -m 2>/dev/null | grep -o 'NV[0-9][0-9]*' | wc -l)
 if [ "$NVLINK_COUNT" -gt 0 ]; then
     HAS_NVLINK=1
@@ -27,51 +30,41 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source "${SCRIPT_DIR}/models/qwen3-4B.sh"
 
 CKPT_ARGS=(
-   --hf-checkpoint /root/Qwen3-4B
+   --hf-checkpoint /mnt/tidal-alsh01/dataset/zeus/hecunjie/models/Qwen/Qwen3-4B-base
    #--hf-checkpoint /root/Qwen3-4B-FP8
-   --ref-load /root/Qwen3-4B_torch_dist
-   --load /root/Qwen3-4B_slime/
-   --save /root/Qwen3-4B_slime/
-   --save-interval 20
+   --ref-load /mnt/tidal-alsh01/dataset/zeus/hecunjie/models/Qwen/Qwen3-4B-base
+   --load /mnt/tidal-alsh01/dataset/zeus/hecunjie/models/Qwen/Qwen3-4B-base
+   --save /mnt/tidal-alsh01/dataset/zeus/hecunjie/train_outputs/slime_kd_plus_rl_test
+   --save-interval 50
 )
 
 ROLLOUT_ARGS=(
-   --prompt-data /root/dapo-math-17k/dapo-math-17k.jsonl
+   --prompt-data /mnt/ali-sh-1/dataset/zeus/hecunjie/rl_data/dapo-math-17k/dapo-math-17k.jsonl
    --input-key prompt
    --label-key label
    --apply-chat-template
    --rollout-shuffle
    --rm-type deepscaler
-   --num-rollout 3000
-   --rollout-batch-size 32
+   # --num-rollout 3000
+   --num-epoch 5
+   --rollout-batch-size 16
    --n-samples-per-prompt 8
    --rollout-max-response-len 8192
    --rollout-temperature 1
 
-   --global-batch-size 256
+   --global-batch-size 128
    --balance-data
 )
 
 EVAL_ARGS=(
    --eval-interval 20
-   --eval-prompt-data aime /root/aime-2024/aime-2024.jsonl
+   --eval-prompt-data aime /mnt/ali-sh-1/dataset/zeus/hecunjie/rl_data/aime24/aime-2024.jsonl
    --n-samples-per-eval-prompt 16
    --eval-max-response-len 16384
    --eval-top-p 1
 )
 
-PERF_ARGS=(
-   --tensor-model-parallel-size 2
-   --sequence-parallel
-   --pipeline-model-parallel-size 1
-   --context-parallel-size 1
-   --expert-model-parallel-size 1
-   --expert-tensor-parallel-size 1
-
-   --recompute-granularity full
-   --recompute-method uniform
-   --recompute-num-layers 1
-
+FSDP_PERF_ARGS=(
    # --micro-batch-size 1
    --use-dynamic-batch-size
    --max-tokens-per-gpu 9216
@@ -79,8 +72,9 @@ PERF_ARGS=(
 
 GRPO_ARGS=(
    --advantage-estimator grpo
+   --train-backend fsdp
    --use-kl-loss
-   --kl-loss-coef 0.00
+   --kl-loss-coef 0.01
    --kl-loss-type low_var_kl
    --entropy-coef 0.00
    --eps-clip 0.2
@@ -89,16 +83,16 @@ GRPO_ARGS=(
 
 DISTILL_ARGS=(
    # Path to the teacher model for distillation
-   --distill-checkpoint /root/Qwen3-72B-Instruct
+   --distill-checkpoint /mnt/tidal-alsh01/dataset/zeus/hecunjie/models/Qwen/Qwen3-32B
    # Coefficient for the distillation loss
-   --distill-coef 0.1
+   --distill-coef 0.3
    # Ratio of top entropy tokens to apply distillation loss (optional)
    --distill-top-entropy-ratio 0.2
 )
 
 OPTIMIZER_ARGS=(
    --optimizer adam
-   --lr 1e-6
+   --lr 5e-6
    --lr-decay-style constant
    --weight-decay 0.1
    --adam-beta1 0.9
@@ -106,10 +100,10 @@ OPTIMIZER_ARGS=(
 )
 
 WANDB_ARGS=(
-   # --use-wandb
-   # --wandb-project slime-dev
-   # --wandb-group qwen3-4B-test
-   # --wandb-key ${WANDB_KEY}
+   --use-wandb
+   --wandb-project slime
+   --wandb-group qwen3-4B-kl
+   --wandb-key ${WANDB_API_KEY}
 )
 
 SGLANG_ARGS=(
@@ -147,14 +141,12 @@ ray job submit --address="http://127.0.0.1:8265" \
    --actor-num-nodes 1 \
    --actor-num-gpus-per-node 8 \
    --colocate \
-   ${MODEL_ARGS[@]} \
    ${CKPT_ARGS[@]} \
    ${ROLLOUT_ARGS[@]} \
    ${OPTIMIZER_ARGS[@]} \
    ${GRPO_ARGS[@]} \
    ${DISTILL_ARGS[@]} \
    ${WANDB_ARGS[@]} \
-   ${PERF_ARGS[@]} \
+   ${FSDP_PERF_ARGS[@]} \
    ${EVAL_ARGS[@]} \
-   ${SGLANG_ARGS[@]} \
-   ${MISC_ARGS[@]}
+   ${SGLANG_ARGS[@]}
